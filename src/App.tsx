653e1e0b0e0f0b54b5cc41b6436e0a4d9cc5bdac
AndreHammel <hammel.engineer@gmail.com>
2022-12-15 10:55:26 -0300

    feat: :sparkles: adiciona formulário com o imput+button e o header que trás informações a quantidade de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import { Header } from './components/Header/Header'
-import './global.css'
-
+import { Header } from "./components/Header/Header";
+import "./global.css";
+import styles from "./App.module.css";
+import plusCircle from "./assets/plusCircle.svg";
 
 export function App() {
-
   return (
-    <div >
+    <div>
       <Header />
-      APP
+      <div className={styles.container}>
+        <form action="" className={styles.form}>
+          <input placeholder="Adicione um nova tarefa" />
+          <button>
+            <span>Criar</span>
+            <img src={plusCircle} />
+          </button>
+        </form>
+        <div className={styles.taskContainer}>
+          <div className={styles.headerTaskContainer}>
+            <div className={styles.infoTasks}>
+              <span id="taskOnBoard">Tarefas criadas</span>
+              <div>0</div>
+            </div>
+            <div className={styles.infoTasks}>
+              <span id="finishedTask">Concluídas</span>
+              <div>0</div>
+            </div>
+          </div>
+          <div className={styles.tasks}>CONTAINER TASKS</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
